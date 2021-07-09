0a7e25e93b443cc25179196d8b33c502e611923b
Nisha2704 <68813245+Nisha2704@users.noreply.github.com>
2021-07-09 20:49:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/app'
+
+
+
+
+
+export  const firebaseConfig = {
+  apiKey: "AIzaSyDVhbrkwXDEQpLQRN4vt6DDDD0hQpFxrM4",
+  authDomain: "story-telling-app-b73ea.firebaseapp.com",
+  databaseURL: "https://story-telling-app-b73ea-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-b73ea",
+  storageBucket: "story-telling-app-b73ea.appspot.com",
+  messagingSenderId: "855290676504",
+  appId: "1:855290676504:web:6bf42383abdc7f869a8a6d"
+};
+  
